3920816181b7e32961b1e35d0714794840c6e6dd
Elaksi <eetun.email@gmail.com>
2021-08-20 13:28:50 +0300

    Style nav bar

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,9 +3,10 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect,
+  NavLink,
 } from "react-router-dom";
+
 import { routes } from "./routes";
 
 export default function Routes() {
@@ -13,10 +14,16 @@ export default function Routes() {
     <Router>
       <div>
         <nav>
-          <ul>
+          <ul className="flex gap-6 justify-center mx-auto my-2">
             {routes.map((route) => (
               <li className="text-lg">
-                <Link to={route.path}>{route.label}</Link>
+                <NavLink
+                  to={route.path}
+                  className="text-blue-500 hover:text-blue-800"
+                  activeClassName="font-bold"
+                >
+                  {route.label}
+                </NavLink>
               </li>
             ))}
           </ul>
