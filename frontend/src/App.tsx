30b785ed3a729df68a95df3a72c8bfeba52f1b83
Elaksi <eetun.email@gmail.com>
2021-08-20 13:34:32 +0300

    Add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,12 @@ function App() {
         <meta charSet="utf-8" />
         <title>Tamtron Measurement App</title>
       </Helmet>
+      <header className="mt-7 mb-3 text-center">
+        <div className="text-4xl font-bold">Tamtron Weighing App</div>
+        <div className="text-xl mt-2">
+          For extremely accurate weighing results
+        </div>
+      </header>
       <Routes />
     </div>
   );
