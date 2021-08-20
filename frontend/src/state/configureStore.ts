4f393bc5be3b890a9eaea41f2f35746da5fd546a
Elaksi <eetun.email@gmail.com>
2021-08-20 11:49:44 +0300

    Add redux rebugger and support for async thunks to redux

--- frontend/src/state/configureStore.ts
+++ frontend/src/state/configureStore.ts
@@ -1,8 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+  Middleware,
+} from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
 
-export const store = configureStore({
-  reducer: {},
-});
+import { measurementSlice } from "./measurement/slice";
+
+const rootReducer = combineReducers({ measurement: measurementSlice.reducer });
+
+function configureStore(preloadedState = undefined) {
+  const middlewares: Middleware[] = [thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+
+  return store;
+}
+
+export const store = configureStore();
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
